5ed56cdada7ccf6e74dbf2a48b6f9519d6b58414
moonhunter <vdcloayza@gmail.com>
2021-07-29 11:04:46 -0400

    declarative rendering

--- 1.js
+++ 1.js
@@ -1,6 +1,16 @@
 const app=new Vue({
     el:'#App0',
     data:{
-        message:'Hello Vue World'
+        message:'Hello Vue World',
+        verduras:[
+            {nombre:'Manzana',cantidad:4},
+            {nombre:'Banana',cantidad:3},
+            {nombre:'Zandia',cantidad:0},
+            {nombre:'repollo',cantidad:0},
+        ],
+        seen:true,
+        
     }
 })
+const arra= [1,2,3,4];
+console.log(arra);
