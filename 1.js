f9e58acb6abf7f541d68e971679833039105d34c
moonhunter <vdcloayza@gmail.com>
2021-07-31 01:14:38 -0400

    haciendo practica todo-list con vue

--- 1.js
+++ 1.js
@@ -1,8 +1,12 @@
 const App0={
     data(){
         return{
+            titulo:'TAREAS PENDIENTES',
             message:'Hello Vue World',
             message2:'putoskys',
+            fondo:'bg-warning',
+            tareas:[],
+            nuevaTarea:'',
             verduras:[
                 {nombre:'Manzana',cantidad:4},
                 {nombre:'Banana',cantidad:3},
@@ -17,6 +21,18 @@ const App0={
     
 },
 methods:{
+    agregarTarea:function(){
+this.tareas.push({
+    nombre:this.nuevaTarea,
+    estado:false,
+})
+console.log(this.tareas);
+this.nuevaTarea='';
+    },
+    editarTarea:function(index){
+// console.log("edits",index);
+this.tareas[index].estado=true;
+    },
     reverseMessage:function(){
         this.message2=this.message2.split('').reverse().join('')
 
